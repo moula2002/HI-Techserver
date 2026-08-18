3324e68338ce71f6c6ffa22d75079af5d00ce635
moula2002 <iammoulahussain@gmail.com>
2026-08-18 18:10:36 +0530

    Add Banners model and API endpoints

--- models/Banner.js
+++ models/Banner.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const bannerSchema = new mongoose.Schema({
   image: {
-    type: String
+    type: String,
   },
   video: {
-    type: String
+    type: String,
   }
 }, { timestamps: true });
 
